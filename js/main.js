47cd085191f4d9411797267fc457ec8aeaecec53
vadym_lobunets <vadym.lobunets@gmail.com>
2021-08-17 11:28:17 +0300

    change

--- js/main.js
+++ js/main.js
@@ -97,27 +97,3 @@ $('.g_popup').on('afterChange', function(event, slick, currentSlide, nextSlide) 
     }
     
 });
-
-      
-    // var tag = document.createElement('script');
-
-    //   tag.src = "https://www.youtube.com/iframe_api";
-    //   var firstScriptTag = document.getElementsByTagName('script')[0];
-    //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-    //   var player;
-    //   function onYouTubeIframeAPIReady() {
-    //     player = new YT.Player('player', {
-    //       height: '360',
-    //       width: '640',
-    //       videoId: 'M7lc1UVf-VE',
-    //       events: {
-    //         'onReady': onPlayerReady,
-    //       }
-    //     });
-    //   }
-
-    //   function onPlayerReady(event) {
-    //     event.target.playVideo();
-    //   }
